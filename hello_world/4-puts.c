5c00ba701a1c0673522306a9781105166b42d404
Pandor3 <thatdumbphilosopher@gmail.com>
2024-06-17 11:54:30 +0000

    4th strike

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+/* The main function is vital for a C code, we can call other functions with it.*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
