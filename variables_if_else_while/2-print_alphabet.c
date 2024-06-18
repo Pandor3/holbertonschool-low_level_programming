4f530f91ed4348b6186e4ba932fc3de309dcf860
Pandor3 <thatdumbphilosopher@gmail.com>
2024-06-18 13:08:49 +0000

    3rd strike

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - is the entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+char ch = 'a';
+for (ch = 'a'; ch <= 'z'; ch ++)
+	putchar(ch);
+
+	return (0);
+}
