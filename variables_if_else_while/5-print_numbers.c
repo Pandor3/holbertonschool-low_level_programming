6d9bea5580e566df8eee97cca09bd5a179146faa
Pandor3 <thatdumbphilosopher@gmail.com>
2024-06-19 08:41:14 +0000

    added 5th exercise

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - is the entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char n = '0';
+
+	for (n = '0'; n <= '9'; n++)
+{
+	putchar(n);
+}
+{
+	putchar('\n');
+}
+	return (0);
+}
