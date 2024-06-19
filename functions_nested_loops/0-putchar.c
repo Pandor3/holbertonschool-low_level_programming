00d451b8930d1c937c3d8c3e97dbf2b09babc548
Pandor3 <thatdumbphilosopher@gmail.com>
2024-06-19 13:01:09 +0000

    did the 1st exercise

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - is the entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char str[15] = "_putchar";
+
+	printf("_putchar");
+	putchar('\n');
+	return (0);
+}
