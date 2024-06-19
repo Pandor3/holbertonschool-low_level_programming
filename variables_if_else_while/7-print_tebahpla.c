e43607c8928b822734bd2ed6ad154782b515b295
Pandor3 <thatdumbphilosopher@gmail.com>
2024-06-19 09:35:48 +0000

    added 7th exercise

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - is the entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char n = 'z';
+	char i = 'a';
+
+	for (n = 'z'; n >= 'a'; n--)
+{
+	putchar(n);
+}
+{
+	putchar('\n');
+}
+	return (0);
+}
