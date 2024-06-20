bcd1a199c2dd1fbfe7f69a680d60472d51410399
Pandor3 <thatdumbphilosopher@gmail.com>
2024-06-20 16:08:58 +0000

    5th exercise

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -7,17 +7,17 @@
 
 int print_sign(int n)
 	{
-	if (n > 0)
+	if (n > 48)
 {
 	return (1);
 	_putchar(43);
 }
-	if (n == 0)
+	if (n == 48)
 {
 	return (0);
 	_putchar(48);
 }
-	if (n < 0)
+	if (n < 48)
 {
 	return (-1);
 	_putchar(45);
