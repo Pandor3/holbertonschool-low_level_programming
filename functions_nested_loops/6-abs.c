d9994d2b53c79cdb07f6405652ed845cedf6bb48
Pandor3 <thatdumbphilosopher@gmail.com>
2024-06-20 19:41:23 +0000

    6th exercise

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - compute the absolute value of the integer
+ * @r: the integer who needs to be computed
+ * Return: Always r ou -r
+ */
+
+int _abs(int r)
+{
+	if (r >= 0)
+{
+	return (r);
+}
+	else
+{
+	return (-r);
+}
+}
