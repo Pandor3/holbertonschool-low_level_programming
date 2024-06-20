b0c077a6e1bb475265c346696623df5d8f2627d2
Pandor3 <thatdumbphilosopher@gmail.com>
2024-06-20 13:02:35 +0000

    3rd exercise

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -7,10 +7,14 @@
 void print_alphabet_x10(void)
 {
 char n;
+int chiffre;
 
-for (n = 'a'; n <= 'z'; n++)
+for (chiffre = '0'; chiffre <= 9; chiffre++)
 {
-_putchar((n) * 10);
-}
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar(n);
+	}
 _putchar('\n');
+}
 }
