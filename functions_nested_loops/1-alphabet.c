708640053c803e9b8b76ed97dfdd7be54c600a73
Pandor3 <thatdumbphilosopher@gmail.com>
2024-06-20 12:28:44 +0000

    2n exercise

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - is the entry point
+ * void print_alphabet(void) - print alphabet
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
