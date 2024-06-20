4f1d3f02dbdc3baa8a29d88ec4d0917b9391f79c
Pandor3 <thatdumbphilosopher@gmail.com>
2024-06-20 12:55:09 +0000

    3rd exercise

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,16 @@
 #include "main.h"
 /**
- * print_alphabetx10 - print the alphabet 10x times
+ * print_alphabet_x10 - print the alphabet 10x times
  * Return: Always 0
  */
 
-void print_alphabet_x10(void);
+void print_alphabet_x10(void)
 {
 char n;
 
 for (n = 'a'; n <= 'z'; n++)
 {
-sum = (n * 10);
-_putchar(n);
+_putchar((n) * 10);
 }
 _putchar('\n');
 }
