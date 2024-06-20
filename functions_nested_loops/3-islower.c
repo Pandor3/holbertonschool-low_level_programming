294b2200abef56dc5d1f3a3dbdb7bb4abe1fc790
Pandor3 <thatdumbphilosopher@gmail.com>
2024-06-20 13:52:17 +0000

    4th exercise

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -2,12 +2,13 @@
 /**
  * _islower - checks if character is lowercase
  * @c: character to be verified
- * Return: Always 0
+ * Return: Always 0 or 1
  */
 
 int _islower(int c)
 {
-if (c <= 'a' && c >= 'z')
+
+if (c >= 'a' && c <= 'z')
 return (1);
 else
 return (0);
