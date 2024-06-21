e4a658bd844eb0cc2078d115af6a4a51f61c112c
Pandor3 <Thatdumbphilosopher@gmail.com>
2024-06-21 11:09:05 +0000

    10th exercise

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - adds two integers and returns the result
+ * @L: is the first integer
+ * @M: is the second integer
+ * Return: Always 0
+ */
+
+int add(int L, int M)
+{
+	return (L + M);
+}
