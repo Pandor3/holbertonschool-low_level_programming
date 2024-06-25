ce7180d0ba866f0b251ae5f45dc82741b9a8f9b6
Pandor3 <thatdumbphilosopher@gmail.com>
2024-06-25 12:01:16 +0000

    added 3rd exercise

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts - prints a string
+ * @str: is the characters
+ */
+
+void _puts(char *str)
+{
+	char i;
+
+	for (c = 0; str[i] != '\0' ; i++)
+{
+	_putchar(str[i]);
+}
+	_putchar ('\n');
+}
