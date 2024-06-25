b3c4101accc0ed193bbab90de02f2a1654ffa922
Pandor3 <thatdumbphilosopher@gmail.com>
2024-06-25 10:30:11 +0000

    Added 1st exercise

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - takes a pointer and update its value
+ * @n: is the pointer and integer
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,8 @@
+#ifndef Main
+#define Main
+
+int _putchar(char c);
+void reset_to_98(int *n);
+
+
+#endif
