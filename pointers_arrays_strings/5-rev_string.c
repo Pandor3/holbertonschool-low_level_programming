09361070c0fd0e6867b53da98d21acfde00244c3
Pandor3 <thatdumbphilosopher@gmail.com>
2024-06-25 13:56:56 +0000

    fin de journee

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,1 +1,23 @@
 #include "main.h"
+/**
+ * rev_string - a function which reverses a string
+ * @s: is the character
+ */
+
+void rev_string(char *s)
+{
+	int a;
+
+	int b;
+
+	for (a = '0'; s[a] != '\0'; a++)
+{
+	_putchar(s[a]);
+	_putchar('\n');
+}
+	for (b = 9 - 1; b > '\0'; b--)
+{
+	_putchar(s[b]);
+}
+	_putchar('\n');
+}
