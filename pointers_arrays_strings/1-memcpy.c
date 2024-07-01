42b95a8a0bdc2ed4e22dabe49cae6dfbff38556d
Pandor3 <thatdumbphilosopher@gmail.com>
2024-07-01 11:45:52 +0000

    added 2 exercises

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,6 +1,19 @@
 #include "main.h"
-
+/**
+ * _memcpy - function which copies a memory area
+ * @dest: pointer on destination
+ * @src: pointer on source
+ * @n: bytes
+ * Return: Destination
+ */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
+	unsigned int i;
 
+	for (i = 0; i < n; i++)
+{
+	dest[i] = src[i];
+}
+	return (dest);
+}
---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strchr - A function which locates a character
+ * @s: pointer to the string
+ * @c: the character to locate
+ * Return: Always NULL or c
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (i < c)
+{
+
+	if (i != c)
+{
+	return ('\0');
+}
+	else
+
+	return (s);
+	i++;
+}
+}
