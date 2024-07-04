fcfb5e00323d2d8bfc968deb37f9c21dd0df1e11
Pandor3 <thatdumbphilosopher@gmail.com>
2024-07-04 13:47:02 +0000

    Added 1st exercise

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,4 +1,29 @@
 #include <stdio.h>
+/**
+ * main - function which prints a file name
+ * @argc: counts arguments
+ * @argv: counts strings
+ * Return: Name of the file or 0
+ */
 
 
+int main(int argc, char *argv[])
+{
+	int count;
 
+	printf("%s\n", argv[0]);
+
+	if (argc > 1)
+	{
+		for (count = 1; count < argc; count++)
+	{
+		printf("argv[%d] = %s\n", count, argv[count]);
+	}
+	}
+	else
+	{
+		return (0);
+	}
+
+		return (0);
+}
