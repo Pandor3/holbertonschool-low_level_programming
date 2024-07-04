8cd80ac51c25a9e19f62fe1b3ad8c8580ff6842d
Pandor3 <thatdumbphilosopher@gmail.com>
2024-07-04 13:57:06 +0000

    added 2nd exercise

---
+++ argc_argv/1-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+int main(int argc, char *argv[])
+{
+	int count;
+	printf("\%s\n", argv[0]);
+
+	if (arg > 1)
+	{
+		for (count = 1; count < argc; count++)
+	{
+		printf("argc[%d] = %s\n", count, argc[count]);
+	}
+	}
+	else
+	{
+		return (0);
+	}
+		return (0):
+}
