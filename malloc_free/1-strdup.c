2b0b81655010958f5c5709901f48ccedbf3d6266
Pandor3 <thatdumbphilosopher@gmail.com>
2024-07-08 12:11:05 +0000

    did 2 exercises

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - returns a pointer which copies a string
+ * @str: pointer to the string
+ * Return: NULL or ptr
+ */
+
+char *_strdup(char *str)
+{
+	int n;
+	int i = 0;
+	char *ptr;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (n = 0; n != '\0'; n++)
+	{
+	}
+
+	ptr = malloc(sizeof(char) * n);
+	if (ptr == 0)
+	{
+		return (NULL);
+	}
+	while (str[i] != '\0')
+	{
+		ptr[i] = str[i];
+		i++;
+	}
+	return (ptr);
+}
