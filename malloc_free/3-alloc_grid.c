6adad1fc8781b8bd6ac28bb4ad4df8e7ee8944e4
Pandor3 <thatdumbphilosopher@gmail.com>
2024-07-09 09:39:09 +0000

    added a few files

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * alloc_grid - function with a pointer to a 2D array of integers
+ * @width: integer for longueur
+ * @height: integer for hauteur
+ * Return: NULL or ptr
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int *ptr;
+	int i;
+	int j;
+
+	if (width < 0 || height < 0)
+	{
+	return (NULL);
+	}
+
+	ptr = malloc(sizeof(int *) * height);
+	for (i = 0; i < height; i++)
+	{
+		ptr[i] = malloc(sizeof(int) * width);
+		for (j = 0; j < width; j++)
+		{
+			ptr[i][j] = 0;
+		}
+	}
+	return (ptr);
