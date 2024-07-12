725c070ff5c8ff64b70cdabe490edf1657dc5eb4
Pandor3 <thatdumbphilosopher@gmail.com>
2024-07-12 07:51:01 +0000

    edited 2nd exercise

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -25,10 +25,10 @@ char *str_concat(char *s1, char *s2)
 	{
 		s2 = "";
 	}
-	for (l1 = 0; s1[l1] < '\0'; l1++)
+	for (l1 = 0; s1[l1] != '\0'; l1++)
 	{
 	}
-	for (l2 = 0; s2[l2] < '\0'; l2++)
+	for (l2 = 0; s2[l2] != '\0'; l2++)
 	{
 	}
 	result = result + l1 + l2 + 1;
