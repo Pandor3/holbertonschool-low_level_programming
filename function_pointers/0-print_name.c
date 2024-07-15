c5b1acdfc879a02967abdbd2eb8414b2d0801e3b
Pandor3 <thatdumbphilosopher@gmail.com>
2024-07-15 09:18:41 +0000

    maybe

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_name - function which prints a name
+ * @name: pointer to main
+ * @f: pointer to main
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == 0)
+	{
+		return;
+	}
+	if (name == 0)
+	{
+		return;
+	}
+	f(name);
+}
