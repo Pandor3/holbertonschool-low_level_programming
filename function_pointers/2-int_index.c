fc3d73ee09c44d9ab2416d0b313338fc8fa8d597
Pandor3 <thatdumbphilosopher@gmail.com>
2024-07-15 12:57:41 +0000

    added 3rd exercise

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,35 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - function which searches for an integer
+ * @array: array
+ * @size: number of elements
+ * @cmp: pointeur de fonction
+ * Return: -1, i or 0
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+	int result;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	while (i <= size)
+	{
+		result = cmp(*array);
+	if (result != 0)
+	{
+		break;
+	}
+	if (i == size)
+	{
+		return (-1);
+	}
+		i++;
+		array++;
+	}
+	return (i);
+}
