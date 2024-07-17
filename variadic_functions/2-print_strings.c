cdd1a71d1526c59add9587a14f42b81d55e43662
Pandor3 <thatdumbphilosopher@gmail.com>
2024-07-17 12:34:52 +0000

    edited 2nd exercise

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -8,16 +8,26 @@
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	unsigned char c;
+	unsigned int i = 0;
 	va_list list;
+	const char *c;
 
 	va_start(list, n);
-	while (c < n)
+	while (i < n)
 	{
-		printf("%s", va_arg(list, char);
-	if (*separator != NULL && c < n - 1);
-	printf("%s", *separator);
+		c = va_arg(list, const char*);
+			if (c == NULL)
+			{
+				printf("(nil)");
+			}
+			else
+			{
+		printf("%s", c);
+			}
+	if (separator != NULL && i < n - 1)
+	printf("%s", separator);
+	i++;
 	}
 	va_end(list);
-	printf("/n");
+	printf("\n");
 }
