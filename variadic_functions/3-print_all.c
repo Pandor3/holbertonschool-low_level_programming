a37b6a8e292e27824bc983d04f680438f5234e52
Pandor3 <Thatdumbphilosopher@gmail.com>
2024-07-18 10:56:17 +0000

    Edited a few files

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -5,3 +5,47 @@
 void print_all(const char * const format, ...)
 {
 
+
+
+
+char letter(void)
+{
+	char a = 65;
+	
+	if (a >= 126 || a < 65)
+		return (NULL);
+	while (a < 126)
+	{
+		printf("%c\n", a)
+	}
+	return (0);
+}
+
+int number(void)
+{
+	int n = 48;
+
+	if (n > 57 || n < 48)
+		return (NULL);
+	while (n <= 57)
+	{
+		printf("%d\n", n)
+	}
+	return (0);
+}
+
+float virgule(void)
+{
+
+}
+
+void string(void)
+{
+	int i = 0;
+	
+	while (i < '\0')
+	{
+		i++;
+	}
+	printf("%s\d", i);
+}
