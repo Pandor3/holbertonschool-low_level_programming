6cccec8b3100476ab2b7aa66cc648eb5180e2257
Pandor3 <Thatdumbphilosopher@gmail.com>
2024-07-30 08:34:19 +0000

    Edited exercise 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,11 +1,27 @@
 #include "lists.h"
 
+/**
+ * print_list - Function which prints the elements in list_t
+ * @h: The elements in list_t
+ * Return: The number of arguments in list_t
+ */
 
 size_t print_list(const list_t *h)
 {
-	int count = 0;
+	size_t count = 0;
 
-	if (str == NULL)
+	while (h != NULL)
+	{
+	if (h->str == NULL)
+	{
 		printf("[0] (nil)\n");
-
-
+	}
+	else
+	{
+		printf("[%d] %s\n", h->len, h->str);
+	}
+	h = h->next;
+	count++;
+	}
+	return (count);
+}
