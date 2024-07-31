bd7241b4d5b71bfaf9ebab6e03355dbf172fb255
Pandor3 <Thatdumbphilosopher@gmail.com>
2024-07-31 13:17:48 +0000

    Edited 3rd exercise

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - Adds a node at the end of the list
+ * @head: pointer to the list
+ * @n: content of the list
+ * Return: NULL or fin
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *nouveau;
+	dlistint_t *fin;
+
+	if (head == NULL)
+		return (NULL);
+
+	nouveau = malloc(sizeof(dlistint_t));
+	if (nouveau == NULL)
+		return (NULL);
+	nouveau->n = n;
+	nouveau->next = NULL;
+
+	if (*head == NULL)
+	{
+		nouveau->n = n;
+		nouveau->next = NULL;
+		nouveau->prev = NULL;
+		*head = nouveau;
+		return (nouveau);
+	}
+	else
+	{
+		fin = *head;
+
+	while (fin->next != NULL)
+	{
+		fin->next = nouveau;
+		nouveau->prev = fin;
+	}
+	}
+	return (nouveau);
+}
