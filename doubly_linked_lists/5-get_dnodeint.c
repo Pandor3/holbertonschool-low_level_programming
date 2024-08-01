8e354ea7f7fdfda659f6aad1f1f8ead1f3bf9caf
Pandor3 <thatdumbphilosopher@gmail.com>
2024-08-01 11:15:22 +0000

    Edited 5th exercise

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -14,7 +14,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	if (head == NULL)
 		return (NULL);
 
-	while (count < index)
+	while (count != index)
 	{
 		if (head->next == NULL && count < index)
 			return (NULL);
