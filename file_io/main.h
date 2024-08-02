8840fcc47ce5d633da2838f552b4b1ca2c20fe8d
Pandor3 <thatdumbphilosopher@gmail.com>
2024-08-02 12:44:51 +0000

    Added Required files

---
+++ file_io/main.h
@@ -0,0 +1,11 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <sys/stat.h>
+#include <stdarg.h>
+
+int _putchar(char c);
+
+#endif
