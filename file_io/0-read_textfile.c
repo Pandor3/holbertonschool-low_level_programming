9a841cdc1be9bb8bae0f74b8d4e8f8d0b1ae99a3
Pandor3 <thatdumbphilosopher@gmail.com>
2024-08-03 10:24:45 +0000

    Edited main.h and added exercise 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,20 +1,45 @@
 #include "main.h"
 
-
+/**
+ * read_textfile - Function which will read and print output
+ * @filename: pointer to the filename chosen
+ * @letters: size of the buffer
+ * Return: -1, 0 or ecrire
+ */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int i;
-	unsigned int count;
+	char *buffer;
+	ssize_t lire;
+	ssize_t ecrire;
 
 	if (filename == NULL)
 		return (0);
-	i = open(*filename, O_RDONLY);
-	if (count == -1)
+	i = open(filename, O_RDONLY);
+	if (i == -1)
 		return (0);
-	for (count = 0; count != filename; count ++)
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
 	{
+		close(i);
+		return (0);
 	}
-	close (i);
-	return (count);
+	lire = read(i, buffer, letters);
+	if (lire == -1)
+	{
+		free(buffer);
+		close(i);
+		return (0);
+	}
+	ecrire = write(STDOUT_FILENO, buffer, lire);
+	if (ecrire == -1 || ecrire != lire)
+	{
+		free(buffer);
+		close(i);
+		return (0);
+	}
+	free(buffer);
+	close(i);
+	return (ecrire);
 }
--- file_io/main.h
+++ file_io/main.h
@@ -3,8 +3,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
-#include <sys/stat.h>
 #include <stdarg.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <elf.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
